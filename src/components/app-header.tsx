5d0ba8909b9196f82ba91407818beb8724f44020
sereno <serenomdominguez@gmail.com>
2025-09-01 11:08:24 -0400

    minimize maximize

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -4,23 +4,28 @@ import {
   HeaderGlobalBar,
   HeaderGlobalAction,
   Theme,
+  Tooltip,
 } from "@carbon/react";
-import { Close, InformationSquare } from "@carbon/icons-react";
+import {
+  CloseLarge,
+  DownToBottom,
+  Information,
+  Maximize,
+  Minimize,
+  UpToTop,
+} from "@carbon/icons-react";
 import { exit } from "@tauri-apps/plugin-process";
 import { useStore } from "../utils/store";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 export const AppHeader = () => {
   const { setCurrentView } = useStore();
+  const appWindow = getCurrentWindow();
 
   return (
     <Theme theme="g90">
       <Header data-tauri-drag-region>
-        <HeaderName
-          data-tauri-drag-region
-          prefix=""
-          style={{ cursor: "grab" }}
-          // onClick=
-        >
+        <HeaderName data-tauri-drag-region prefix="">
           <HeaderGlobalAction onClick={() => setCurrentView("main")}>
             <p style={{ color: "#f4f4f4" }}>lala</p>
           </HeaderGlobalAction>
@@ -30,11 +35,25 @@ export const AppHeader = () => {
             data-tauri-drag-region
             onClick={() => setCurrentView("about")}
           >
-            <InformationSquare size={20} />
+            <Information size={18} />
           </HeaderGlobalAction>
-          <HeaderGlobalAction data-tauri-drag-region onClick={() => exit(0)}>
-            <Close size={20} />
+          <HeaderGlobalAction
+            data-tauri-drag-region
+            onClick={() => appWindow.minimize()}
+          >
+            <DownToBottom size={15} />
           </HeaderGlobalAction>
+          <HeaderGlobalAction
+            data-tauri-drag-region
+            onClick={() => appWindow.maximize()}
+          >
+            <UpToTop size={15} />
+          </HeaderGlobalAction>
+          <Tooltip label="close app">
+            <HeaderGlobalAction data-tauri-drag-region onClick={() => exit(0)}>
+              <CloseLarge size={20} />
+            </HeaderGlobalAction>
+          </Tooltip>
         </HeaderGlobalBar>
       </Header>
     </Theme>
